88caf8315afe0e59af3fdced9de2a3b1643b176d
zfarmertic <lilyigbunu@gmail.com>
2024-08-09 19:45:22 +0100

    merit

---
+++ quiz.js
@@ -0,0 +1,15 @@
+
+const checker = (event) =>{
+    console.log (event.target.value)
+}
+clearInterval
+const getUserInput = (radioNamed) =>{
+
+let radioName = document.getElementsByClassName(radioNamed)
+    console.log(radioName)
+    // for(let i = 0; i<radioName.length; i++){
+    //     radioName[i].addEventListener('change', checker)
+    // }
+}
+
+getUserInput(nigeria)
