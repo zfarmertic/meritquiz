ba0b5fcac6f4e548ee47a2345350b4aa7cb11330
zfarmertic <lilyigbunu@gmail.com>
2024-08-14 02:48:45 +0100

    new git

--- quiz.js
+++ quiz.js
@@ -1,15 +1,47 @@
+// let right = document.getElementsByClassName("right")
+// const newRight = Array.from(right)
+// const total = []
 
+// newRight.forEach((element, index)=>{
+//     const num = (element.getAttribute('data-num'))
+//     element.addEventListener('click', ()=>{
+//         total[num] = true
+
+//         console.log(total)
+//     })
+   
+// })
+let right = document.getElementsByClassName('right')
+let answers = [...right]
+console.log(answers)
 const checker = (event) =>{
-    console.log (event.target.value)
+    for(let i = 0; i<answers.length; i++){
+        let answerArray = []
+        let userAnswer = event.target.value;
+        console.log(userAnswer);
+        if(userAnswer === answers[i]){
+            answerArray.push(i)
+            console.log(answerArray)
+        }
+        else{
+            return null
+        }
+        console.log(answerArray)
+    
+    }
+    
 }
-clearInterval
+
+
 const getUserInput = (radioNamed) =>{
-
-let radioName = document.getElementsByClassName(radioNamed)
-    console.log(radioName)
-    // for(let i = 0; i<radioName.length; i++){
-    //     radioName[i].addEventListener('change', checker)
-    // }
+let radioName = document.getElementsByName(radioNamed)
+    for(let i = 0; i<radioName.length; i++){
+        radioName[i].addEventListener('change', checker)
+    }
 }
 
-getUserInput(nigeria)
+getUserInput("nigeria")
+getUserInput("ghana")
+getUserInput("cotonou")
+getUserInput("asia")
+getUserInput("cyprus")
